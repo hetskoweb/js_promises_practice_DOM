0f08146f1c7eb074a3f9ceedf6cac8c0050500af
Illia <hetsko.ilya@gmail.com>
2025-03-23 01:46:47 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,80 @@
 'use strict';
+
+const notification = document.createElement('div');
+
+document.body.appendChild(notification);
+
+let isResolved = false;
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    if (!isResolved) {
+      isResolved = true;
+      resolve('First promise was resolved');
+    }
+  });
+
+  setTimeout(() => {
+    if (!isResolved) {
+      reject(new Error('First promise was rejected'));
+    }
+  }, 3000);
+});
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('mousedown', (e) => {
+    if (e.buttons === 1 || e.buttons === 2) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+const thirdPromise = new Promise((resolve, reject) => {
+  let firstButtonPressed = false;
+
+  document.addEventListener('mousedown', (e) => {
+    if (e.buttons === 1) {
+      firstButtonPressed = 'left';
+    } else if (e.buttons === 2) {
+      firstButtonPressed = 'right';
+    }
+
+    if (
+      (e.buttons === 1 && firstButtonPressed === 'right') ||
+      (e.buttons === 2 && firstButtonPressed === 'left')
+    ) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+function promises() {
+  firstPromise
+    .then((result) => {
+      notification.textContent = result;
+      notification.classList.add('success');
+    })
+    .catch((error) => {
+      notification.textContent = error.message;
+      notification.classList.add('error');
+    });
+
+  secondPromise
+    .then((result) => {
+      notification.textContent = result;
+      notification.classList.add('success');
+    })
+    .catch((error) => {
+      notification.textContent = error.message;
+      notification.classList.add('error');
+    });
+
+  thirdPromise
+    .then((result) => {
+      notification.textContent = result;
+      notification.classList.add('success');
+    })
+    .catch((error) => {
+      notification.textContent = error.message;
+      notification.classList.add('error');
+    });
+}
+
+promises();
